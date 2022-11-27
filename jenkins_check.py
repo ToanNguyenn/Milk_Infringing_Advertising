48f0205f9c6dea08d612cd085ab124e5bf712784
ToanNguyenn <88270091+ToanNguyenn@users.noreply.github.com>
2022-11-27 18:14:42 +0700

    Check tele bot

--- jenkins_check.py
+++ jenkins_check.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello world"
+    return "Update, Hello world"
 if __name__ == '__main__':
     print("App run!!!")
     app.run(debug=True)
